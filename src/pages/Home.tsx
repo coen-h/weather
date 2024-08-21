6d0ab62b636652c5cab2d83934c186e476003e22
coen-h <105825347+coen-h@users.noreply.github.com>
2024-08-21 17:57:21 +1200

    added icons, first page element

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center flex-col">
-      <h1 className="text-6xl font-bold">Main Page</h1>
-      {/* https://open-meteo.com/en/docs */}
-      {/* https://www.creative-tim.com/twcomponents/cheatsheet */}
+    <div className="w-screen h-screen p-6">
+      <div className="flex w-full h-full bg-slate-300 drop-shadow-md rounded-lg">
+        <div className="flex flex-col relative p-4 pr-32 m-4 bg-white rounded-lg h-min">
+          <img src="thunder.svg" alt="Sunny" className="absolute w-48 h-48 -top-8 -right-6" />
+          <h1 className="text-3xl font-bold">Auckland</h1>
+          <p className="text-lg font-light">Tue, Aug 21</p>
+          <p className="text-8xl font-light">21°C</p>
+        </div>
+      </div>
     </div>
   );
 }
+{/* https://open-meteo.com/en/docs */}
+{/* https://www.creative-tim.com/twcomponents/cheatsheet */}
